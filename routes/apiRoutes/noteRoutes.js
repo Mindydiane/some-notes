f6cfcbadeb3752f4e6e777603eb25c3d6fdba533
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-08-14 18:27:13 -0500

    api routes established

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const {
+  filterByQuery,
+  findById,
+  createNewNote,
+  validateNote,
+} = require("../../lib/notes");
+const { notes } = require("../../db/db.json");
+
+//add a route, get method requires two arguements
+router.get("/notes", (req, res) => {
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  console.log(results);
+  res.json(results);
+});
+
+//get a new route for notes by id
+router.get("/notes/:id", (req, res) => {
+  const result = findById(req.params.id, notes);
+  console.log(result);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+// post or create notes
+router.post("/notes", (req, res) => {
+  // set id based on what the next index of the array will be
+  //   req.body.id = notes.length.toString();
+  console.log(notes);
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send("The note is not properly formatted.");
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+module.exports = router;
